ee155f9168a6572ebbb8a41347f51f118bd2929f
dhariya-siddhant <dhariya.siddhant@gmail.com>
2023-03-30 00:52:05 +1000

    integrating with AWS amplify

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsExports = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_32vHN3ZAz",
-    "aws_user_pools_web_client_id": "24of414bljj0g4atsd5stjt7m0",
+    "aws_user_pools_id": "us-east-1_h2ywljSGe",
+    "aws_user_pools_web_client_id": "r6kh6uh623p080u70a23gj8ps",
   };
   
   export default awsExports;
